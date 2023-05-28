66efe83ce295c6833429fdfa988e1773bf9e47a1
Hyeon9mak <jinha3507@gmail.com>
2023-05-28 19:06:38 +0900

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-boot-circuit-breaker-playground"
---
+++ src/test/kotlin/com/example/springbootcircuitbreakerplayground/SpringBootCircuitBreakerPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootcircuitbreakerplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootCircuitBreakerPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
